dea3f077b898b1c302a2e2128de99ec4cbb1e4a0
Effin <effinjoewilson@gmail.com>
2024-08-17 12:47:46 +0530

    Integrate client-side Socket.io for real-time room interactions Connected the Room component to the server using Socket.io for real-time communication. Added lifecycle methods to manage the connection.

--- client/src/routes/room/Room.js
+++ client/src/routes/room/Room.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
-const Room = () => (
-    <div className="room">
-        <h1>Welcome to the Room</h1>
-    </div>
-);
+const Room = () => {
+    useEffect(() => {
+        const socket = io();
+
+        socket.on('connect', () => {
+            console.log('Connected to server');
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    return (
+        <div className="room">
+            <h1>Welcome to the Room</h1>
+        </div>
+    );
+};
 
 export default Room;
